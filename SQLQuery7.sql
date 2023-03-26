24e8dc87ee002e199a2422fd56cd4ec925050153
luqman574 <63543999+luqman574@users.noreply.github.com>
2023-03-26 10:03:18 +0400

    Add files via upload

---
+++ SQLQuery7.sql
@@ -0,0 +1,31 @@
+ create table customer
+(firstname varchar(50),
+lastname varchar(50),
+age int
+)
+insert into customer
+(firstname,lastname,[age])
+values ('guy','pierce',50);
+
+insert into customer
+(firstname,lastname,[age])
+values ('Brad','Pitt',55);
+
+insert into customer
+(firstname,lastname,[age])
+values ('Helen','Mirren',60);
+
+insert into customer
+(firstname,lastname,[age])
+values ('Jay','Bird',43);
+
+insert into customer
+(firstname,lastname,[age])
+values ('Sissy','Speck',88);
+
+insert into customer
+(firstname,lastname,[age])
+values ('Sissy','Speck2',88);
+insert into customer
+(firstname,lastname,[age])
+values ('Sissy','Speck2',88);
